4092f297e20660ad71c19f9c5712b05c759ba6e1
Dave Lewis <siu06dl@gmail.com>
2024-10-29 16:36:09 +0000

    feat: initial commodity implementation

---
+++ Components/Commodities/Commodity.cs
@@ -0,0 +1,17 @@
+﻿namespace Spacelancer.Components.Commodities;
+
+public sealed class Commodity
+{
+    public CommoditySize Size { get; private set; }
+    public CommodityType Type { get; private set; }
+    public int Quantity { get; private set; }
+    public int PurchasePrice { get; private set; }
+
+    public Commodity(CommoditySize size, CommodityType type, int quantity, int purchasePrice)
+    {
+        Size = size;
+        Type = type;
+        Quantity = quantity;
+        PurchasePrice = purchasePrice;
+    }
+}
