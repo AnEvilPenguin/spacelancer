83b9e09cb9d76d101c60c2b961f08bd28b146aa9
Dave Lewis <siu06dl@gmail.com>
2024-10-29 17:22:23 +0000

    feat: basic cargo hold implementation

---
+++ Scenes/Player/PlayerCargoHold.cs
@@ -0,0 +1,9 @@
+﻿using Spacelancer.Components.Commodities;
+using Spacelancer.Components.Storage;
+
+public partial class Player
+{
+    public CargoHold Hold = new CargoHold(CommoditySize.Medium, 100);
+
+    public int Credits = 500;
+}
