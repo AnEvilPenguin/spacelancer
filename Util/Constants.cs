b73fc49c5eb8acb1d274acec0516872fd371d5b7
dave <siu06dl@gmail.com>
2024-10-31 18:50:55 +0000

    fix: dodgy log path on nix

--- Util/Constants.cs
+++ Util/Constants.cs
@@ -4,6 +4,6 @@ internal static class Constants
 {
     public static readonly string FolderPath = Path.Combine(
         System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData),
-        "EvilPenguinIndustries\\Spacelancer"
+        "EvilPenguinIndustries","Spacelancer"
     );
 }
