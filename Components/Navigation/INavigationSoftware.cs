245a03dfb6b963c4b668c1ba2ae5b6c3ddc4dc5c
Dave Lewis <siu06dl@gmail.com>
2024-11-22 19:03:11 +0000

    doc: plans around breaking dependencies

--- Components/Navigation/INavigationSoftware.cs
+++ Components/Navigation/INavigationSoftware.cs
@@ -15,7 +15,9 @@ public interface INavigationSoftware
     public string Name { get; }
     public NavigationSoftwareType Type { get; }
     
+    // FIXME consider taking in current rotation/position(?) to break dependency on ship
     public float GetRotation(float maxRotation);
     
+    // FIXME consider taking in current velocity/position to break dependency on ship
     public Vector2 GetVelocity(float maxSpeed);
 }
