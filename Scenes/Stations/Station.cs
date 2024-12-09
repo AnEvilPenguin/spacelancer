414c7ace57bffe25f4bea38c335064b4fe2dc96d
Dave Lewis <siu06dl@gmail.com>
2024-12-09 18:34:37 +0000

    style: remove unnecessary type declaration

--- Scenes/Stations/Station.cs
+++ Scenes/Stations/Station.cs
@@ -28,21 +28,19 @@ public partial class Station : Node2D, IDockable, ISensorDetectable
 		"TODO";
 
 	// We may need to consider making this docking range if we make a map and remote comms or something
-	private bool _playerInCommsRange = false;
+	private bool _playerInCommsRange;
 	private StationMenu _menu;
 	
-	private readonly List<Tuple<Commodity, int>> _commoditiesForSale = new List<Tuple<Commodity, int>>();
-	private readonly Dictionary<string, int> _commodityBuyPriceOverride = new Dictionary<string, int>();
+	private readonly List<Tuple<Commodity, int>> _commoditiesForSale = new ();
+	private readonly Dictionary<string, int> _commodityBuyPriceOverride = new ();
 	
-	private readonly List<NonPlayerCharacter> _nonPlayerCharacters = new List<NonPlayerCharacter>();
+	private readonly List<NonPlayerCharacter> _nonPlayerCharacters = new ();
 	
 	private IdentificationFriendFoe _iff;
 	private TrafficController _trafficController;
 
 	public override void _Ready()
 	{
-		var stationBorder = GetNode<Area2D>("Area2D");
-		
 		_iff = new IdentificationFriendFoe(this);
 		AddChild(_iff);
 		
@@ -87,7 +85,7 @@ public partial class Station : Node2D, IDockable, ISensorDetectable
 	}
 	
 	public List<NonPlayerCharacter> GetNonPlayerCharacters() => 
-		new List<NonPlayerCharacter>(_nonPlayerCharacters);
+		new (_nonPlayerCharacters);
 	
 	public bool HasNpc() =>
 		_nonPlayerCharacters.Count > 0;
@@ -109,7 +107,7 @@ public partial class Station : Node2D, IDockable, ISensorDetectable
 		_commodityBuyPriceOverride.Add(commodity.Name, price);
 
 	public List<Tuple<Commodity, int>> GetCommodityForSale() => 
-		new List<Tuple<Commodity, int>>(_commoditiesForSale);
+		new (_commoditiesForSale);
 
 	public int GetCommodityToBuyPrice(Commodity commodity)
 	{
